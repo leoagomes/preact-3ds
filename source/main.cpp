eb862c0ad196e4715dcd60fa1adf016dc85253ef
Leonardo G <lgomes.leo@gmail.com>
2019-08-04 03:29:48 -0300

    httpc request state tests

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 #include <sys/time.h>
 
 #include <iostream>
+#include <memory>
 
 #include "duktape.h"
 
@@ -43,6 +44,40 @@ int main(int argc, char* argv[]) {
     }
     duk_pop(context);
 
+    httpcContext http;
+
+    auto open_result = httpcOpenContext(&http, HTTPC_METHOD_GET,
+        "http://httpstat.us/200?sleep=7000", 0);
+    if (R_FAILED(open_result)) {
+        printf("error opening http context.\n");
+    }
+    if (R_FAILED(httpcAddRequestHeaderField(&http, "Accept", "application/json"))) {
+        printf("error setting accept header field.\n");
+    }
+    if (R_FAILED(httpcBeginRequest(&http))) {
+        printf("error beginning request.\n");
+    }
+
+    loop->register_timer(std::make_shared<events::timer>(
+        false,
+        1000,
+        [&http] (u64 id, void* ud) {
+            HTTPC_RequestStatus status;
+            if (R_FAILED(httpcGetRequestState(&http, &status))) {
+                printf("failed getting request state.\n");
+                return;
+            }
+            printf("request status: %x\n", status);
+
+            if (status == HTTPC_STATUS_DOWNLOAD_READY) {
+                printf("download_ready\n");
+            }
+            if (status == HTTPC_STATUS_REQUEST_IN_PROGRESS) {
+                printf("request in progress\n");
+            }
+        }
+    ));
+
     while (aptMainLoop())
     {
         gspWaitForVBlank();
@@ -56,8 +91,9 @@ int main(int argc, char* argv[]) {
         loop->cycle();
     }
 
-    delete loop;
+    httpcCloseContext(&http);
 
+    delete loop;
     terminate();
     return 0;
 }
@@ -66,9 +102,11 @@ void initialize() {
     gfxInitDefault();
     consoleInit(GFX_TOP, NULL);
     romfsInit();
+    httpcInit(0x2000);
 }
 
 void terminate() {
+    httpcExit();
     romfsExit();
     gfxExit();
 }
